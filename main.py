363ab3af06a42064f1691bed900e6f396ce35a81
Anupam Krishna <anupamkris13262@gmail.com>
2024-01-18 21:28:15 +0530

    tinyrest

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route("/api", methods=["GET"])
+def api():
+    return jsonify({"message": "Hello World!"})
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
+
